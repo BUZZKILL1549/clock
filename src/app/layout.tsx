c922dcb75646eed5470bafab4b4193b6c8823fbf
Aushadh Subramanian <aushvs10@hotmail.com>
2025-06-09 09:54:56 +0530

    Add analytics properly

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import { Analytics } from "@vercel/analytics";
+import { Analytics } from "@vercel/analytics/next";
 import { ThemeProvider } from "../components/theme-provider";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
